8945fa4e547b1f629f51f9f8537a619a89428226
Neha Devwanshi <nehadevwanshi@gmail.com>
2017-02-09 12:42:19 -0800

    Facebook user details

--- demo/src/common/sidebar.js
+++ demo/src/common/sidebar.js
@@ -185,7 +185,7 @@ export default class SidebarContainer extends React.Component {
                 <img src='/imgs/app/avatars/avatar0.png' width='40' height='40' />
               </Col>
               <Col xs={8} collapseLeft id='avatar-col'>
-                <div style={{top: 23, fontSize: 16, lineHeight: 1, position: 'relative'}}>Anna Sanchez</div>
+                <div style={{top: 23, fontSize: 16, lineHeight: 1, position: 'relative'}}>Neha Devwanshi</div>
                 <div>
                   <Progress id='demo-progress' value={30} color='#ffffff'/>
                   <Link to={::this.getPath('lock')}>
